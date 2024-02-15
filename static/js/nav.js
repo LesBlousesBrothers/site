caf414c0575b9578c9f33ac8571c6a31fe799fa2
p-mathis <pmathis@protonmail.com>
2024-02-15 22:42:21 +0100

    menu

--- static/js/nav.js
+++ static/js/nav.js
@@ -6,12 +6,16 @@ const nav = document.getElementById("nav")
 triggerMenuOpen.addEventListener("click", () => {
     // nav.classList.add("flex")
     nav.classList.remove("hidden");
-    triggerMenuClosed.style.display ="block";
+    //triggerMenuClosed.style.display ="block";
+    triggerMenuClosed.classList.remove("hidden");
+    triggerMenuClosed.classList.add("block");
     triggerMenuOpen.style.display = "none";
 });
 triggerMenuClosed.addEventListener("click", () => {
     nav.classList.add("hidden");
     // nav.classList.remove("flex")
     triggerMenuOpen.style.display ="block";
-    triggerMenuClosed.style.display = "none";
+    //triggerMenuClosed.style.display = "none";
+    triggerMenuClosed.classList.remove("block");
+    triggerMenuClosed.classList.add("hidden");
 });
