e5a2e7d9442475347b6e922ee19263b879cc3e07
p-mathis <pmathis@protonmail.com>
2024-04-13 18:13:49 +0200

    suppression du fichier public

--- public/js/showEvents.js
+++
@@ -1,9 +0,0 @@
-  function showAgenda() {
-    let agendaGoogle = document.getElementById("agendaGoogle");
-    let agenda = document.getElementById("agenda");
-    agendaGoogle.display = "none";
-    agenda.display = "block";
-
-  }
-
-  showAgenda()
